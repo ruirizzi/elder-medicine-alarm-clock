6e4a30637e083a38580847f274e7b8520dae080d
Rui Rizzi <rui.rizzi@intemobile.com>
2019-06-21 10:38:58 -0300

    Schedule object lab

--- Tests/test.py
+++ Tests/test.py
@@ -1,17 +1,33 @@
-class Robot():
-    """A Robot class"""
-    def __init__(self, name, color, weight):
-        self.name = name
-        self.color = color
-        self.weight = weight
-    
-    def introduce_self(self):
-        print("My name is " + self.name)
+from enum import Enum
+import time
+import datetime
+class Schedule():
+    def __init__(self, dayOfWeek, hour, minute):
+        self.dayOfWeek = dayOfWeek
+        self.hour = hour
+        self.minute = minute
 
 
-    
-r1 = Robot("Jorge", "Verde", 30)
-r1.introduce_self()
+class WeekDays(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
 
-r2 = Robot("Lucas", "Verde", 30)
-r2.introduce_self()
+schedList = []
+sched = Schedule(WeekDays.FRIDAY.value, 10, 39)
+schedList.append(sched)
+sched = Schedule(WeekDays.FRIDAY.value, 10, 38)
+schedList.append(sched)
+sched = Schedule(WeekDays.FRIDAY.value, 10, 40)
+schedList.append(sched)
+
+while True:
+    time.sleep(5)
+    now = datetime.datetime.today()
+    for s in schedList:
+        if s.dayOfWeek == now.weekday() and s.hour == now.hour and s.minute ==  now.minute:
+            pass
